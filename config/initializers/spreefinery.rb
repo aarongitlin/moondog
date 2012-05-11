385d40295265e0dd21c23640b79809a4ae62c596
Rohan Mitchell <rohan@rohanmitchell.com>
2012-05-11 20:22:35 +1000

    Register refinery_user? as a helper method

--- config/initializers/spreefinery.rb
+++ config/initializers/spreefinery.rb
@@ -1,3 +1,4 @@
+# -- Patch will_paginate for compatibility with kaminari
 if defined?(WillPaginate)
   module WillPaginate
     module ActiveRecord
@@ -7,4 +8,11 @@ if defined?(WillPaginate)
       end
     end 
   end
+end
+
+
+# -- Register refinery_user? as a helper method
+# For some reason this is missed by refinerycms-authentication-2.0.3/lib/refinery/authenticated_system.rb
+ApplicationController.class_eval do
+  helper_method :refinery_user?
 end
