abaf5f4a2f619b1a3fdae664d0ddd78016f43382
Rohan Mitchell <rohan@rohanmitchell.com>
2012-05-18 11:03:30 +1000

    Set up preliminary editing of beer locations

--- vendor/extensions/beer_locations/app/models/refinery/beer_locations/beer_location.rb
+++ vendor/extensions/beer_locations/app/models/refinery/beer_locations/beer_location.rb
@@ -3,6 +3,8 @@ module Refinery
     class BeerLocation < Refinery::Core::BaseModel
       self.table_name = 'refinery_beer_locations'      
     
+      attr_accessible :name, :address, :latitude, :longitude, :position
+
       acts_as_indexed :fields => [:name, :address]
 
       validates :name, :presence => true, :uniqueness => true
