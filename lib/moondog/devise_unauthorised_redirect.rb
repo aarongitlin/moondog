7466289eabd87c3e767e7a412d6c6b9dbe8db9d1
Rohan Mitchell <rohan@rohanmitchell.com>
2012-05-27 09:09:02 +1000

    Comment devise_unauthorised_redirect

--- lib/moondog/devise_unauthorised_redirect.rb
+++ lib/moondog/devise_unauthorised_redirect.rb
@@ -1,12 +1,12 @@
-# When a user hits a page that they're unauthorised to access,
-# redirect them to the spree login page
-
 module Moondog
   class CustomFailure < Devise::FailureApp
+    # When a user hits a page that they're unauthorised to access, redirect them to the spree login page
     def redirect_url
       spree.login_path
     end
 
+    # Visiting /refinery results in a return_to path of "//refinery", which when redirected to,
+    # lands the user at http://refinery. To avoid this, remove the duplicate '/'.
     def store_location!
       session["#{scope}_return_to"] = attempted_path.gsub('//', '/') if request.get? && !http_auth?
     end
