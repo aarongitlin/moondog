224efae37968327a4e6bb6fc153ab361e3be6172
Rohan Mitchell <rohan@rohanmitchell.com>
2012-05-26 12:18:36 +1000

    Set up admin form for currently brewing

--- vendor/extensions/brewing_beers/app/models/refinery/brewing_beers/brewing_beer.rb
+++ vendor/extensions/brewing_beers/app/models/refinery/brewing_beers/brewing_beer.rb
@@ -3,7 +3,14 @@ module Refinery
     class BrewingBeer < Refinery::Core::BaseModel
       self.table_name = 'refinery_brewing_beers'      
 
+      # TODO: Add proper labels
+      DARKNESS_OPTIONS = ['0 Pretty light!', '1', '2', '3', '4', '5', '6', '7', '8', '9 Tar-black']
+      BITTERNESS_OPTIONS = ['0 Mild', '1', '2', '3', '4', '5', '6', '7', '8', '9 Hoptastic']
+      ABV_OPTIONS = ['0.5 - 1.5% de-alcoholised', '1.5 - 4% light', '4 - 6 % standard', '6+% alco']
+
       acts_as_indexed :fields => [:name, :description]
+
+      attr_accessible :name, :description, :darkness, :bitterness, :abv, :is_displayed, :position
 
       validates :name, :presence => true, :uniqueness => true
               
