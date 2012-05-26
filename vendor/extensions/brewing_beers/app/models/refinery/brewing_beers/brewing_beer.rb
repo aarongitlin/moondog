46305b2ba8e6ecd3f525836481f85b72d849c8ee
Rohan Mitchell <rohan@rohanmitchell.com>
2012-05-26 15:54:10 +1000

    Convert brewing beer field labels to hash structure for frontend display

--- vendor/extensions/brewing_beers/app/models/refinery/brewing_beers/brewing_beer.rb
+++ vendor/extensions/brewing_beers/app/models/refinery/brewing_beers/brewing_beer.rb
@@ -3,16 +3,18 @@ module Refinery
     class BrewingBeer < Refinery::Core::BaseModel
       self.table_name = 'refinery_brewing_beers'      
 
-      # TODO: Add proper labels
-      DARKNESS_OPTIONS = ['0 Pretty light!', '1', '2', '3', '4', '5', '6', '7', '8', '9 Tar-black']
-      BITTERNESS_OPTIONS = ['0 Mild', '1', '2', '3', '4', '5', '6', '7', '8', '9 Hoptastic']
-      ABV_OPTIONS = ['0.5 - 1.5% de-alcoholised', '1.5 - 4% light', '4 - 6 % standard', '6+% alco']
+      FIELD_LABELS = {:darkness => {0 => '0 Pretty light!', 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9 Tar-black'},
+                      :bitterness => {0 => '0 Mild', 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9 Hoptastic'},
+                      :abv => {0 => '0.5 - 1.5% de-alcoholised', 3 => '1.5 - 4% light', 6 => '4 - 6 % standard', 9 => '6+% alco'}}
+
 
       acts_as_indexed :fields => [:name, :description]
 
       attr_accessible :name, :description, :darkness, :bitterness, :abv, :is_displayed, :position
 
       validates :name, :presence => true, :uniqueness => true
+
+      scope :displayed, where(:is_displayed => true)
               
     end
   end
