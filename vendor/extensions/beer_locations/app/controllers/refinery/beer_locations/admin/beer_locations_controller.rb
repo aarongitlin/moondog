9d0e6a5e05a73f4b5398c76912b60c4116445f74
Rohan Mitchell <rohan@rohanmitchell.com>
2012-05-27 11:15:08 +1000

    Make marker draggable, update lat and lng on drop

--- vendor/extensions/beer_locations/app/controllers/refinery/beer_locations/admin/beer_locations_controller.rb
+++ vendor/extensions/beer_locations/app/controllers/refinery/beer_locations/admin/beer_locations_controller.rb
@@ -2,10 +2,16 @@ module Refinery
   module BeerLocations
     module Admin
       class BeerLocationsController < ::Refinery::AdminController
+        before_filter :prepare_map_json, :only => [:new, :edit]
 
         crudify :'refinery/beer_locations/beer_location',
                 :title_attribute => 'name', :xhr_paging => true
 
+        def prepare_map_json
+          @beer_location_json = @beer_location.to_gmaps4rails do |beer_location, marker|
+            marker.json({:title => 'Drag to reposition', :draggable => true})
+          end
+        end
       end
     end
   end
