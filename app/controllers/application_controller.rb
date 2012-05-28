0a832caf44b6dd6ecad6e43c329aa605effffecf
Rohan Mitchell <rohan@rohanmitchell.com>
2012-05-29 09:33:05 +1000

    Display currently brewing beers on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_brewing_beers
+
+  def load_brewing_beers
+    @brewing_beers = Refinery::BrewingBeers::BrewingBeer.displayed
+  end
+
 end
