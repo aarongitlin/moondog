112bf13fd77aa07a302625048e02669e35d96a0e
Aaron Gitlin <aarongitlin@gmail.com>
2012-08-01 18:41:56 +1000

    changed brewing beers SRM to numbers

--- vendor/extensions/brewing_beers/app/models/refinery/brewing_beers/brewing_beer.rb
+++ vendor/extensions/brewing_beers/app/models/refinery/brewing_beers/brewing_beer.rb
@@ -3,7 +3,7 @@ module Refinery
     class BrewingBeer < Refinery::Core::BaseModel
       self.table_name = 'refinery_brewing_beers'      
 
-      FIELD_LABELS = {:darkness => {0 => 'Pale lager', 1 => 'German Pilsener', 2 => 'Pilsener Urquell', 4 => 'Weissbier', 5 => 'Bass pale ale', 6 => 'Dark lager', 7 => 'Porter', 8 => 'Stout', 9 => 'Imperial stout'},
+      FIELD_LABELS = {:darkness => {0 => '2 SRM', 1 => '8 SRM', 2 => '15 SRM', 4 => '20 SRM', 5 => '25 SRM', 6 => '30 SRM', 7 => '35 SRM', 8 => '45 SRM', 9 => '55 SRM', 10 => '60+ SRM'},
                       :abv => {0 => '0.5-2%', 1 => '2-4% light', 2 => '4-6% standard', 3 => '6-7%', 4 => '7-8%', 5 => '8-9%', 6=> '9-10%', 7 => '10-11%', 8 => '11-12%', 8.5 => '12-13%', 9 => '13-14%', 9.5 => '14-15%', 10 => 'over 15%'}}
 
 
