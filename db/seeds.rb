c71e4015b683134e3db497cbf10c6d075b58d45b
Aaron Gitlin <aarongitlin@gmail.com>
2012-11-20 09:03:23 +1100

    commented out rohans seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,16 +39,20 @@ end
 
 # -- Spreefinery
 # Grant Refinery roles to any Spree admins
-admin_role = Spree::Role.find_by_name('admin')
-refinery_role = Spree::Role.where(:name => 'Refinery').first_or_create!
-refinery_superuser_role = Spree::Role.where(:name => 'Superuser').first_or_create!
+# admin_role = Spree::Role.find_by_name('admin')
+# refinery_role = Spree::Role.where(:name => 'Refinery').first_or_create!
+# refinery_superuser_role = Spree::Role.where(:name => 'Superuser').first_or_create!
+# 
+# admin_role.users.uniq.each do |user|
+#   [refinery_superuser_role, refinery_role].each do |role|
+#     if user.has_role? role.name
+#       puts "[db:seed] Skipping Refinery #{role.name} role for user #{user.email}"
+#     else
+#       user.roles << role
+#     end
+#   end
+# end
 
-admin_role.users.uniq.each do |user|
-  [refinery_superuser_role, refinery_role].each do |role|
-    if user.has_role? role.name
-      puts "[db:seed] Skipping Refinery #{role.name} role for user #{user.email}"
-    else
-      user.roles << role
-    end
-  end
-end
+
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
