5eacf48430e8030e26a91ab4aea7b62145556f97
Jonas Rademar <jonas.rademar@gmail.com>
2013-01-06 10:17:16 +0000

    Modified Spree layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   before_filter :load_brewing_beers
   before_filter :load_videos
 
+  helper Spree::BaseHelper
+  helper Spree::CheckoutHelper
+  helper Spree::ProductsHelper
+  helper Spree::TaxonsHelper
+
   def load_brewing_beers
     @brewing_beers = Refinery::BrewingBeers::BrewingBeer.displayed
   end
