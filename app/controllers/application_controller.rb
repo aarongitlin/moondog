6cfc3ff2e6dd1d2834806395c5a8c24d1163bc5f
Jonas Rademar <jonas.rademar@gmail.com>
2013-01-12 13:07:27 +0000

    Top Position video to show on homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,15 @@ class ApplicationController < ActionController::Base
   helper_method :refinery_user?
   
   before_filter :load_brewing_beers
+  before_filter :load_videos
 
   def load_brewing_beers
     @brewing_beers = Refinery::BrewingBeers::BrewingBeer.displayed
+  end
+  
+
+  def load_videos
+    @videos = Refinery::Videos::Video.displayed
   end
 
 end
