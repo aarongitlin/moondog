9d17ecb1c4d80a3d839867c813dd02677b2b7c05
Jonas Rademar <jonas.rademar@gmail.com>
2013-01-18 03:59:41 +0000

    Australia as default country & hide cartimage when no cart

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,9 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+    
+  # Sets default country to Australia
+  config.default_country_id = 12
 end
 
 Spree.user_class = "Refinery::User"
