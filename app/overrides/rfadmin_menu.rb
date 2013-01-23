432d22c107034bbcf22a78268d4fcc807b593795
Aaron Gitlin <aarongitlin@gmail.com>
2013-01-23 22:51:37 +1100

    put settings back in refinery

--- app/overrides/rfadmin_menu.rb
+++ app/overrides/rfadmin_menu.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "refinery/admin/_menu", 
                      :name => "rfadmin_menu", 
                      :surround => "code[erb-loud]:contains('refinery.url_for(plugin.url)')",
-                     :text => "<% if plugin.title != 'Images' && plugin.title != 'Files' && plugin.title != 'Settings' %><% render_original %><% end %>")
+                     :text => "<% if plugin.title != 'Images' && plugin.title != 'Files' %><% render_original %><% end %>")
 Deface::Override.new(:virtual_path => "refinery/admin/_menu", 
                      :name => "rm_reorder_rfadmin_menu", 
                      :remove => "code[erb-silent]:contains('active.in_menu.many?')",
